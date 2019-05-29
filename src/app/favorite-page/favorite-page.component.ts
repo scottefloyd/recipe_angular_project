b8d669f6afd0f2fb55aa15c46c6fd1fcd786a015
Brittany Hardy <behardy25@gmail.com>
2019-05-29 11:39:33 -0400

    fixed calories

--- src/app/favorite-page/favorite-page.component.ts
+++ src/app/favorite-page/favorite-page.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./favorite-page.component.css']
 })
 export class FavoritePageComponent implements OnInit {
+favList: any[] =[];
 
   constructor() { }
 
